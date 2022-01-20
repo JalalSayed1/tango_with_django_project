9733ff9bb1f31a14fe5c2938a5c33ded766a17e9
Jalal Sayed <mrjalalsayed@gmail.com>
2022-01-20 16:58:33 +0000

    Chapter 3 is done

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,5 @@ app_name = 'rango'
 urlpatterns = [
     # empty string URL will be passed from tango_with_django project if URL included rango/
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
