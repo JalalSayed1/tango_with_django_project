cc57943db4699f7f51641747b36724cd11cfb64c
Jalal Sayed <mrjalalsayed@gmail.com>
2022-01-21 19:34:54 +0000

    Chapter 4 finished - without the exercises

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from rango import views
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    # return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': ' Crunchy, creamy, cookie, candy, cupcake!'}
+    # return a rendered response to send to the client:
+    # NB: the first para is the template we wish to use
+    return render(request, 'rango/index.html', context= context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,18 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print()
+# print(__file__)
+# print()
+# print(os.path.dirname(__file__))
+# print()
+# print(os.path.dirname(os.path.dirname(__file__)))
+
+# dir to where my templates are
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +67,9 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        
+        'DIRS': [TEMPLATE_DIR],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,10 +77,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # to obtain a reference to MEDIA_URL path when we need to reference uploaded content
             ],
         },
     },
 ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
@@ -114,8 +132,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+# data structure to where django can find my static files:
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
