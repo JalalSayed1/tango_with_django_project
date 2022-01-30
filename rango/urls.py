5f3735033e6c98bc6f1e2d6bff48447973634589
Jalal Sayed <mrjalalsayed@gmail.com>
2022-01-30 07:43:56 +0000

    Chapter 6 untill the demo

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # empty string URL will be passed from tango_with_django project if URL included rango/
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    # category_name_slug is the same var name we passed in show_category() in views.py
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
