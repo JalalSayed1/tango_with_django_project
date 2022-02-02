7978b34bbf26784e089b833b74823c5c577f545c
Jalal Sayed <mrjalalsayed@gmail.com>
2022-02-02 10:31:21 +0000

    Chapter 6 done - tests passed

--- rango/views.py
+++ rango/views.py
@@ -9,11 +9,15 @@ def index(request):
     # Query the database for a list of ALL categories currently stored. # Order the categories by the number of likes in descending order. # Retrieve the top 5 only -- or all if less than 5. # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     
+    # - sign means from top to bottom:
     category_list = Category.objects.order_by('-likes')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    
+    top_pages = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = top_pages
     
     # return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
     # context_dict = {'boldmessage': ' Crunchy, creamy, cookie, candy, cupcake!'}
